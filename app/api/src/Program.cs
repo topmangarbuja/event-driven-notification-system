6db0627670fc23bf807a6cdd2996b24e8296f948
Topman Garbuja <it.topmangarbuja@gmail.com>
2026-06-06 14:12:24 +1000

    feat(api): remove weather forecast endpoint and related code

--- app/api/src/Program.cs
+++ app/api/src/Program.cs
@@ -14,25 +14,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.MapPost("/api/send-messages", (SendMessagesRequest request) =>
 {
     // Here you would typically handle the incoming message, e.g., save it to a database or send it to another service.
@@ -41,10 +22,5 @@ app.MapPost("/api/send-messages", (SendMessagesRequest request) =>
 });
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
 
 public record SendMessagesRequest(string FullName, string Message, string Mobile, string Email);
